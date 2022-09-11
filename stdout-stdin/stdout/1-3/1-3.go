15a05247d9a2feaa58e54c633edd866212316501
Takuya Yokoi <takuya.y.211540@gmail.com>
2022-09-12 08:14:37 +0900

    remove stdou-stdin old dir

--- stdout-stdin/stdout/1-3/1-3.go
+++
@@ -1,8 +0,0 @@
-package main
-import (
-    "fmt"
-)
-
-func main() {
-    fmt.Println(27182 % 818)
-}
--- stdout-stdin/stdout/1-5/1-5.go
+++
@@ -1,8 +0,0 @@
-package main
-import (
-    "fmt"
-)
-
-func main() {
-    fmt.Println(24*60*60)
-}
