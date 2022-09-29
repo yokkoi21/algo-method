ca7353394f5f6fb6487d0e56b648c349a28e641d
Takuya Yokoi <takuya.y.211540@gmail.com>
2022-09-30 00:52:17 +0900

    add regular_expression/look-ahead_look_behind/1.go

---
+++ regular_expression/look-ahead_look_behind/1.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"regexp"
+)
+
+func main() {
+	var S string
+
+	fmt.Scan(&S)
+
+	r := regexp.MustCompile("rur")
+
+	for r.MatchString(S) {
+		S = r.ReplaceAllString(S, "rar")
+	}
+	fmt.Println(S)
+
+}
